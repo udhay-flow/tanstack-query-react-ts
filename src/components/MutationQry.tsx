a66a1561f348f84f4cca6eeac6db53ff6a93424c
Udhay-flow <udhayakumar.t@in.flowglobal.net>
2025-05-28 16:37:19 +0530

    optimistic updates

--- src/components/MutationQry.tsx
+++ src/components/MutationQry.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { postApi } from '../service'
 import LasttnPosts from './LasttnPosts'
+import type { PostProps } from './PgntdQry'
 
 const cmnStyleObj = { padding: "12px", borderRadius: 6 }
 
@@ -14,23 +15,27 @@ const MutationQry = () => {
     // hook
     const queryClient = useQueryClient()
     const { isPending, mutateAsync } = useMutation({
-        mutationFn: (payload: Record<string, string>) => postApi('https://jsonplaceholder.typicode.com/posts', payload),
+        mutationFn: (payload: Record<string, string>) => postApi('https://jsonplaceholder.typicode.com/pots', payload),
         onSuccess: () => {
             alert('Updated successfully')
             setbody("")
             settitle("")
-            queryClient.invalidateQueries({queryKey: ["last_10_posts"]})
+            queryClient.setQueryData(['last_10_posts'], (cached: PostProps) => [...[{ body, title }], ...cached])
         },
-        onError: () => alert('An error occured!!!')
+        onError: () => {
+            // queryClient.setQueryData(['last_10_posts'], (cached: PostProps) => cached.slice(1))
+            alert('An error occured!!!')
+        }
     })
 
     return (
         <div style={{ display: 'flex', gap: '12px' }}>
-            {isPending ? <h1 style={{minWidth: '300px', textAlign: 'center'}}>Mutation happening...</h1> :
+            {isPending ? <h1 style={{ minWidth: '300px', textAlign: 'center' }}>Mutation happening...</h1> :
                 <div style={{ display: 'flex', flexDirection: 'column', padding: '12px', gap: '12px', minWidth: '300px' }}>
                     <input placeholder='Body' style={cmnStyleObj} value={body} onChange={(e) => setbody(e.target.value)} />
                     <input placeholder='Title' style={cmnStyleObj} value={title} onChange={(e) => settitle(e.target.value)} />
-                    <button style={cmnStyleObj} onClick={() => mutateAsync({ body, title })}>Submit</button>
+                    <button style={cmnStyleObj} onClick={() =>
+                        mutateAsync({ body, title })}>Submit</button>
                 </div>
             }
             <LasttnPosts />
